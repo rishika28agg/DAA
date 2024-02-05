6d20549eea8e2c2ced75a67a0d6ef0894925a9e0
Rishika Agarwal <rishikaagarwal2728@gmail.com>
2024-02-05 20:04:48 +0530

    add new file

---
+++ Q1.c
@@ -0,0 +1,3 @@
+/*Problem Statement:
+Given an array of non-negative integers, design a linear algorithm and implement it using a program to find whether given key element is
+present in the array or not. Also, find total number of comparisons for each input case.*/
