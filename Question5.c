cf10656ec157aa7b3bf49177372e28e8da75dacb
Rishika Agarwal <142816310+rishika28agg@users.noreply.github.com>
2024-02-10 08:27:30 +0530

    Update Question5.c

--- Question5.c
+++ Question5.c
@@ -1,3 +1,7 @@
+/*Problem Statement:
+Given a sorted array of positive integers, design an algorithm and implement it using a program to find three indices
+i, j, k such that arr[i] + arr[j] = arr[k].
+*/
 #include<stdio.h>
 #include<stdlib.h>
 int main()
